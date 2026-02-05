b8d2c530122e42e2f2bee6aa83c8eb13619e5eb0
Võ Lâm Tuấn <volamtuan10@gmail.com>
2026-02-05 14:56:42 +0000

    fix Runtime Error
    Invalid src prop (https://thuvien.camau.gov.vn/Conten

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'thuvien.camau.gov.vn',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
